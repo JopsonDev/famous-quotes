8ce99fa8577709dbd437134280419d67e869d48e
JopsonDev <jopsonmason@outlook.com>
2026-04-20 10:23:18 -0400

    added 10 quotes

---
+++ src/main/java/com/pluarlsight/Main.java
@@ -0,0 +1,17 @@
+package com.pluarlsight;
+
+public class Main {
+    static void main() {
+        String[] quotes = {"Be yourself; everyone else is already taken.",
+                "In the end, we only regret the chances we didn’t take.",
+                "Do what you can, with what you have, where you are.",
+                "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+                "Believe you can and you're halfway there.",
+                "The only way to do great work is to love what you do.",
+                "Life is what happens when you're busy making other plans.",
+                "You miss 100% of the shots you don’t take.",
+                "Act as if what you do makes a difference. It does.",
+                "Hardships often prepare ordinary people for an extraordinary destiny."};
+
+    }
+}
